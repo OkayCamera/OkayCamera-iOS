93516698178b38612c0dcf3afd55c80bda32ec01
Zhongqc <zhongqc7@gmail.com>
2017-08-15 01:02:50 +0800

    project init

---
+++ OkayCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OkayCamera
+//
+//  Created by Zhongqc on 2017/8/15.
+//  Copyright © 2017年 OkayCamera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OkayCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OkayCamera
+//
+//  Created by Zhongqc on 2017/8/15.
+//  Copyright © 2017年 OkayCamera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
